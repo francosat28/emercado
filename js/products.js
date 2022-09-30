b157a68cff5cdfb402e7d6eb27375dea29dfd88e
francosat28 <110430417+francosat28@users.noreply.github.com>
2022-09-30 20:26:00 -0300

    asd

--- js/products.js
+++ js/products.js
@@ -90,6 +90,7 @@ function classify(criteria, ordProd){
             return b.soldCount-a.soldCount;
         });
     }
+<<<<<<< Updated upstream
     return result;
 }
 
@@ -108,3 +109,36 @@ function setDogID(id) {
     window.location = "product-info.html"
 }
 
+=======
+}
+
+
+function sortCategories(criteria, array){
+    let result = [];
+    if (criteria === ORDER_ASC_BY_NAME)
+    {
+        result = array.sort(function(a, b) {
+            if ( a.cost < b.cost ){ return -1; }
+            if ( a.cost > b.cost ){ return 1; }
+            return 0;
+        });
+    }else if (criteria === ORDER_DESC_BY_NAME){
+        result = array.sort(function(a, b) {
+            if ( a.cost > b.cost ){ return -1; }
+            if ( a.cost < b.cost ){ return 1; }
+            return 0;
+        });
+    }else if (criteria === ORDER_BY_PROD_COUNT){
+        result = array.sort(function(a, b) {
+            let aCount = parseInt(a.productCount);
+            let bCount = parseInt(b.productCount);
+
+            if ( aCount > bCount ){ return -1; }
+            if ( aCount < bCount ){ return 1; }
+            return 0;
+        });
+    }
+
+    return result;
+}
+>>>>>>> Stashed changes
